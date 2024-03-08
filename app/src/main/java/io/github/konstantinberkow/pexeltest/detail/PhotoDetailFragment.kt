8b3281a05a7e6ebe07f02a34d8ae19b555b9a400
kb <berkow@gmail.com>
2024-03-08 20:52:56 +0200

    Open photographer on click

--- app/src/main/java/io/github/konstantinberkow/pexeltest/detail/PhotoDetailFragment.kt
+++ app/src/main/java/io/github/konstantinberkow/pexeltest/detail/PhotoDetailFragment.kt
@@ -1,6 +1,8 @@
 package io.github.konstantinberkow.pexeltest.detail
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -17,9 +20,12 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import io.github.konstantinberkow.pexeltest.R
 
+
 class PhotoDetailFragment : Fragment() {
 
     private lateinit var photoImageView: ImageView
+
+    private lateinit var authorCard: CardView
 
     private lateinit var authorNameTextView: TextView
 
@@ -44,6 +50,7 @@ class PhotoDetailFragment : Fragment() {
         val layout = inflater.inflate(R.layout.fragment_photo_detail, container, false)
 
         photoImageView = layout.findViewById(R.id.photo_image_view)
+        authorCard = layout.findViewById(R.id.author_card)
         authorNameTextView = layout.findViewById(R.id.author_name_text_view)
         loadProgressBar = layout.findViewById(R.id.load_progress_view)
 
@@ -54,20 +61,22 @@ class PhotoDetailFragment : Fragment() {
         super.onStart()
 
         val photoId = arguments?.getLong(PHOTO_ID_KEY, 0L) ?: 0L
-        if (photoId != 0L) {
-            viewModel.getPhoto(photoId).observe(this) { state ->
-                val photoInfo = state.photoDetail
-                if (photoInfo != null) {
-                    showPhoto(photoInfo)
-                } else {
-                    hideInfo()
-                }
+        if (photoId == 0L) {
+            return
+        }
 
-                loadProgressBar.visibility = if (state.loading || !photoLoaded) {
-                    View.VISIBLE
-                } else {
-                    View.GONE
-                }
+        viewModel.getPhoto(photoId).observe(this) { state ->
+            val photoInfo = state.photoDetail
+            if (photoInfo != null) {
+                showPhoto(photoInfo)
+            } else {
+                hideInfo()
+            }
+
+            loadProgressBar.visibility = if (state.loading || !photoLoaded) {
+                View.VISIBLE
+            } else {
+                View.GONE
             }
         }
     }
@@ -105,11 +114,21 @@ class PhotoDetailFragment : Fragment() {
             .into(photoImageView)
 
         authorNameTextView.text = photo.authorName
-        (authorNameTextView.parent as View).visibility = View.VISIBLE
+        authorCard.visibility = View.VISIBLE
+
+        authorCard.setOnClickListener {
+            openAuthorUrlInBrowser(photo.authorUrl)
+        }
     }
 
     private fun hideInfo() {
-        (authorNameTextView.parent as View).visibility = View.GONE
+        authorCard.visibility = View.GONE
+    }
+
+    private fun openAuthorUrlInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        startActivity(intent)
     }
 
     companion object {

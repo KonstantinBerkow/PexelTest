c853329977c46e4654bf946e7d90e5f634f57e29
kb <berkow@gmail.com>
2024-03-11 22:02:53 +0200

    Remove global deps

--- app/src/main/java/io/github/konstantinberkow/pexeltest/curated/CuratedPhotosViewModelFactory.kt
+++ app/src/main/java/io/github/konstantinberkow/pexeltest/curated/CuratedPhotosViewModelFactory.kt
@@ -1,25 +1,18 @@
 package io.github.konstantinberkow.pexeltest.curated
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
-import io.github.konstantinberkow.pexeltest.app.GlobalDependencies
-
-private const val TAG = "CuratedPhotosViewModelFactory"
+import io.github.konstantinberkow.pexeltest.app.PexelTestApp
 
 object CuratedPhotosViewModelFactory : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         require(modelClass == CuratedPhotosViewModel::class.java)
-        val app = extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
-//        val savedStateHandle = extras.createSavedStateHandle()
-        Log.d(TAG, "App instance: $app")
-//        Log.d(TAG, "Saved state handle: $savedStateHandle")
+        val app = extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PexelTestApp
         return CuratedPhotosViewModel(
-            pexelApiProvider = { GlobalDependencies.pexelApi }
+            pexelApiProvider = { app.dependenciesContainer.pexelApi }
         ) as T
     }
 }
--- app/src/main/java/io/github/konstantinberkow/pexeltest/detail/PhotoDetailViewModelFactory.kt
+++ app/src/main/java/io/github/konstantinberkow/pexeltest/detail/PhotoDetailViewModelFactory.kt
@@ -3,15 +3,17 @@ package io.github.konstantinberkow.pexeltest.detail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-import io.github.konstantinberkow.pexeltest.app.GlobalDependencies
+import io.github.konstantinberkow.pexeltest.app.PexelTestApp
 
 object PhotoDetailViewModelFactory : ViewModelProvider.Factory {
 
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         require(modelClass == PhotoDetailViewModel::class.java)
+        val app = extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PexelTestApp
 
         return PhotoDetailViewModel(
-            pexelApiProvider = { GlobalDependencies.pexelApi }
+            pexelApiProvider = { app.dependenciesContainer.pexelApi }
         ) as T
     }
 }

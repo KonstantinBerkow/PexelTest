1bc15bef424d28b2bd100eeac03b059c579b2043
kb <berkow@gmail.com>
2024-03-11 21:54:17 +0200

    Gather dependencies inside app instance

---
+++ app/src/main/java/io/github/konstantinberkow/pexeltest/app/AppDependenciesContainer.kt
@@ -0,0 +1,68 @@
+package io.github.konstantinberkow.pexeltest.app
+
+import android.content.Context
+import android.graphics.Color
+import android.util.Log
+import com.google.gson.GsonBuilder
+import io.github.konstantinberkow.pexeltest.BuildConfig
+import io.github.konstantinberkow.pexeltest.cache.DbPexelPhotoStore
+import io.github.konstantinberkow.pexeltest.network.PexelApi
+import io.github.konstantinberkow.pexeltest.network.PexelPhoto
+import io.github.konstantinberkow.pexeltest.network.PexelPhotoGsonAdapter
+import io.github.konstantinberkow.pexeltest.network.PexelPhotoPage
+import io.github.konstantinberkow.pexeltest.network.PexelPhotoPageGsonAdapter
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "AppDependenciesContainer"
+
+class AppDependenciesContainer(
+    private val appContext: Context
+) {
+
+    val pexelPhotoStore by lazy {
+        Log.d(TAG, "Create pexelPhotoStore")
+        DbPexelPhotoStore(
+            context = appContext,
+            photoBasePath = "https://images.pexels.com/photos/"
+        )
+    }
+
+    val gson by lazy {
+        Log.d(TAG, "Create gson")
+        val photoAdapter = PexelPhotoGsonAdapter(parseColor = Color::parseColor)
+        val photoPageAdapter = PexelPhotoPageGsonAdapter(photoAdapter)
+        GsonBuilder()
+            .registerTypeAdapter(PexelPhoto::class.java, photoAdapter)
+            .registerTypeAdapter(PexelPhotoPage::class.java, photoPageAdapter)
+            .create()
+    }
+
+    val pexelApi by lazy {
+        Log.d(TAG, "Create okhttp instance")
+        val okHttpClient = OkHttpClient.Builder()
+            .also {
+                if (BuildConfig.DEBUG) {
+                    val loggingInterceptor = HttpLoggingInterceptor { message ->
+                        Log.v("Retrofit", message)
+                    }
+                    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    it.addInterceptor(loggingInterceptor)
+                }
+            }
+            .build()
+
+        Log.d(TAG, "Create retrofit")
+        val retrofitInstance = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://api.pexels.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .validateEagerly(true)
+            .build()
+
+        Log.d(TAG, "Create api instance")
+        retrofitInstance.create(PexelApi::class.java)
+    }
+}
--- app/src/main/java/io/github/konstantinberkow/pexeltest/app/PexelTestApp.kt
+++ app/src/main/java/io/github/konstantinberkow/pexeltest/app/PexelTestApp.kt
@@ -1,6 +1,15 @@
 package io.github.konstantinberkow.pexeltest.app
 
-import androidx.multidex.MultiDexApplication
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 
-class PexelTestApp : MultiDexApplication()
+class PexelTestApp : Application() {
 
+    val dependenciesContainer by lazy { AppDependenciesContainer(this) }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
